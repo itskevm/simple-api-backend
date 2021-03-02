b43cd578179d584f2295d0d1defa83eb7fa97b56
itskevm <kevmgaming@gmail.com>
2021-03-02 16:52:49 -0700

    global var test

--- index.php
+++ index.php
@@ -1,6 +1,47 @@
 <?php
 
+
+
+
   $testvar = "hello world";
   echo $testvar;
+  echo $_SERVER['HTTP_ORIGIN'];
+
+  //$name = $_POST['fname'];
+
+
+/*
+$url = 'https://scheduler.luminarycxm.com/api/v1/cleaned/data/test/';
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "Accept: application/json",
+   "Content-Type: application/json",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+$data = <<<DATA
+{
+  "Id": $_POST[''],
+  "Customer": "Jason Sweet",
+  "Quantity": 1,
+  "Price": 18.00
+}
+DATA;
+
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+var_dump($resp);
+*/
 
 ?>
