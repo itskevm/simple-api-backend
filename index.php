88db5437cf7a4fb5f5c7cc6c6102764ca897f7d9
itskevm <kevmgaming@gmail.com>
2021-03-02 17:05:20 -0700

    add additional cors info

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
   header("Access-Control-Allow-Origin: http://localhost:8080");
   header('Access-Control-Allow-Credentials: true');
-  header('Access-Control-Max-Age: 86400');    // cache for 1 day
+  header('Access-Control-Max-Age: 86400');
+  header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+  header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
 
   $testvar = "hello world";
   echo $testvar;
